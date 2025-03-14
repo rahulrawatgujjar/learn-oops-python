47d328add17a5df253a198f9a74ddd638455ec93
Rahul Rawat <rahulrawatr320@gmail.com>
2025-03-14 22:26:36 +0530

    Learn about method in class

--- file_1.py
+++ file_1.py
@@ -1,22 +1,24 @@
-# create a Car class with attributes like brand and model.
-#   Then create instance of this class.
+# Learn about methods
 
 class Car:
-  # constructor:
-  #   It is used to initialize the objects.
-  #   It is automatically called when an object is created.
+
   def __init__(self,brand,model):
     self.brand=brand
     self.model=model
+
+  # Method:
+  """
+  It is a function which is called for an object of class
+  """
+  def fullName(self):
+    return f"{self.brand} {self.model}"
+  
 
 
 
 if __name__=="__main__":
 
   my_car=Car("Toyota","Fortuner")
-  print(my_car.brand)
-  print(my_car.model)
-
-  my_car_2=Car("Tata","Safari")
-  print(my_car_2.brand)
-  print(my_car_2.model)
+  print(my_car.brand)         # Toyota
+  print(my_car.model)         # Fortuner
+  print(my_car.fullName())    # Toyota Fortuner
